e8f8e47fcd025ae01d0914259bb9a1e6fa093073
Manish-ahirwar <giankumar@GIANs-MacBook-Air.local>
2025-06-21 12:26:59 +0530

    added loops file with some codes

--- loop.js
+++ loop.js
@@ -80,10 +80,136 @@
 // }
 
 // Do While loop ---------->
-let i = 1;
 
-do{
-    console.log(i);
-    i++;  
-}
-while(i <= 6);
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++;  
+// }
+// while(i <= 6);
+
+
+// Some practice questions ----------
+
+// 1) Print even numbers between 1 to 20
+
+//  using for loop
+
+// for( i= 2; i <= 20; i += 2){
+//     console.log(i);
+    
+// }
+
+// using while loop
+
+// let i = 2;
+
+// while( i <= 20){
+//     console.log(i);
+//     i += 2;
+    
+// }
+
+// let i = 2;
+
+// do{
+//     console.log(i);
+//     i += 2;
+// }while( i <= 20)
+
+
+// 3) Sum of numbers from 1 to 100
+
+// using for loop------
+
+// let sum = "0";
+
+// for( let i = 1; i <= 100; i++){
+//     sum += i;
+   
+// }
+//  console.log(i);
+
+
+// using while loop ----
+
+// let  sum = "0";
+// let i = 1;
+
+// while( i <= 100){
+//     sum += i
+//     console.log(sum);
+//     i++;
+// }
+
+
+// 4) Multiplication table of 5
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`5 x ${i} = ${5*i}`);
+    
+// }
+
+// using while loop------
+
+// let i = 1;
+
+// while( i <=10){
+//     console.log(`5 x ${1} = ${5*i}`);
+//     i++;
+    
+// }
+
+// using do while loop ----
+// do{
+//     console.log(`5 x ${i} = ${5*i}`);
+//     i++;
+// }while(i <= 10)
+
+
+// 5) Reverse numbers from 10 to 1
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+// using while loop------
+
+// let i = 10;
+// while( i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+//   <- Indermediate Level ->
+
+// 6) Numbers divisible by 3 and 5 (1 to 100)
+
+// for(let i = 1; i <=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+        
+//     }
+
+// }
+
+// Using while loop -----------
+
+// let i = 1;
+// while( i <= 100){
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i);
+         
+        
+//     }
+//     i++;
+    
+// }
+
+// 7) Factorial of a number (e.g., 5)
+
+
+
+    
